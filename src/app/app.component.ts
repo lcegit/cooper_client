a7d48b6734655f0f5b8c2b47c90c28a26ffec946
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-14 15:44:55 +0100

    Imports, adds and injects Angular2TokenService to the main module, calls init() fn. Adds login and logout buttons to menu and adds corresponding functions. Also adds currentUser to store session user's details.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -15,6 +15,7 @@ export class MyApp {
   rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
+  currentUser: any;
 
   constructor(
     public platform: Platform,
@@ -45,5 +46,54 @@ export class MyApp {
   openPage(page) {
 
     this.nav.setRoot(page.component);
+  }
+
+  loginPopUp() {
+    console.log('popup');
+    let confirm = this.alertCtrl.create({
+      title: 'Login',
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'email'
+        },
+        {
+          name: 'password',
+          placeholder: 'password',
+          type: 'password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Login',
+          handler: data => {
+            this.login(data);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  login(credentials) {
+    this._tokenService
+      .signIn(credentials)
+      .subscribe(
+      res => (this.currentUser = res.json().data),
+      err => console.error('error')
+      );
+  }
+
+  logout() {
+    this._tokenService
+      .signOut()
+      .subscribe(res => console.log(res), err => console.error('error'));
+    this.currentUser = undefined;
   }
 }
