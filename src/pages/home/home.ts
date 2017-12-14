22d39e539c0f33d1fd4545f760d1b7c01ec95d8f
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-14 16:59:18 +0100

    Assessment calc function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { PerfomanceDataProvider } from '../../providers/perfomance-data/perfomance-data';
 import { PersonProvider } from '../../providers/person/person';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
@@ -13,12 +14,18 @@ export class HomePage {
     this.user = { distance: 1000, age: 20, gender: 'female' };
   }
 
-  calculate() {
-    this.person.age = this.user.age;
-    this.person.gender = this.user.gender;
+  // calculate() {
+  //   this.person.age = this.user.age;
+  //   this.person.gender = this.user.gender;
+  //
+  //   this.person.doAssessment(this.user.distance);
+  //   console.log(this.person.assessmentMessage);
+  // }
 
-    this.person.doAssessment(this.user.distance);
-    console.log(this.person.assessmentMessage);
+    calculate(user) {
+    this.person.doAssessment(user.distance);
+    this.perfomanceData
+      .saveData({ performance_data: { data: { message: this.person.assessmentMessage } } })
+      .subscribe(data => console.log(data));
   }
-
 }
